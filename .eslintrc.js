5952183b9d0916e48b17639630467d809e8405de
Iwhite67 <alexis_z@hotmail.fr>
2026-04-10 14:09:48 +0200

    Refactor code structure for improved readability and maintainability

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,15 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
+    "vue/valid-v-slot": "off",
+    "vue/no-useless-template-attributes": "off",
+    "vue/no-mutating-props": "off"
   }
 };
